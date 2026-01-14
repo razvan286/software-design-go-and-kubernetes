d001a698df269f3b409e6ec21a660ca17da0d7de
Razvan <razvan@d-centralize.nl>
2026-01-14 23:54:08 +0100

    35

--- apis/services/auth/mux/mux.go
+++ apis/services/auth/mux/mux.go
@@ -5,6 +5,7 @@ package mux
 import (
 	"os"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/razvan286/software-design-go-and-kubernetes/apis/services/api/mid"
 	"github.com/razvan286/software-design-go-and-kubernetes/apis/services/auth/route/authapi"
 	"github.com/razvan286/software-design-go-and-kubernetes/apis/services/auth/route/checkapi"
@@ -14,10 +15,10 @@ import (
 )
 
 // WebAPI constructs a http.Handler with all application routes bound.
-func WebAPI(log *logger.Logger, auth *auth.Auth, shutdown chan os.Signal) *web.App {
+func WebAPI(build string, log *logger.Logger, db *sqlx.DB, auth *auth.Auth, shutdown chan os.Signal) *web.App {
 	app := web.NewApp(shutdown, mid.Logger(log), mid.Errors(log), mid.Metrics(), mid.Panics())
 
-	checkapi.Routes(app, auth)
+	checkapi.Routes(build, app, log, db)
 	authapi.Routes(app, auth)
 
 	return app
--- apis/services/sales/route/sys/checkapi/route.go
+++ apis/services/sales/route/sys/checkapi/route.go
@@ -1,6 +1,7 @@
 package checkapi
 
 import (
+	"github.com/jmoiron/sqlx"
 	"github.com/razvan286/software-design-go-and-kubernetes/apis/services/api/mid"
 	"github.com/razvan286/software-design-go-and-kubernetes/app/api/authclient"
 	"github.com/razvan286/software-design-go-and-kubernetes/business/api/auth"
@@ -9,13 +10,14 @@ import (
 )
 
 // Routes adds specific routes for this group.
-func Routes(app *web.App, log *logger.Logger, authClient *authclient.Client) {
+func Routes(build string, app *web.App, log *logger.Logger, db *sqlx.DB, authClient *authclient.Client) {
 	authen := mid.AuthenticateService(log, authClient)
 	athAdminOnly := mid.AuthorizeService(log, authClient, auth.RuleAdminOnly)
 
-	app.HandleFuncNoMiddleware("GET /liveness", liveness)
-	app.HandleFuncNoMiddleware("GET /readiness", readiness)
-	app.HandleFunc("GET /testerror", testError)
-	app.HandleFunc("GET /testpanic", testPanic)
-	app.HandleFunc("GET /testauth", liveness, authen, athAdminOnly)
+	api := newAPI(build, log, db)
+	app.HandleFuncNoMiddleware("GET /liveness", api.liveness)
+	app.HandleFuncNoMiddleware("GET /readiness", api.readiness)
+	app.HandleFunc("GET /testerror", api.testError)
+	app.HandleFunc("GET /testpanic", api.testPanic)
+	app.HandleFunc("GET /testauth", api.liveness, authen, athAdminOnly)
 }
